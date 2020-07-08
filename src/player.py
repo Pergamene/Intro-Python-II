05ace51f97eae6016164b2702e90a69c4f66ba03
Pergamene <smartz373@gmail.com>
2020-07-08 15:15:49 -0600

    day 1 mvp

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+  def __init__(self, name, current_room):
+    self.name = name
+    self.current_room = current_room
+
+  def set_current_room(self, current_room):
+    self.current_room = current_room
+
+
+  # Players able to carry multiple items
--- src/room.py
+++ src/room.py
@@ -1,2 +1,30 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+    self.n_to: None
+    self.s_to: None
+    self.e_to: None
+    self.w_to: None
+
+  def get_name(self):
+    return self.name
+
+  def get_description(self):
+    return self.description
+    
+  def get_n_to(self):
+    return self.n_to
+
+  def get_s_to(self):
+    return self.s_to
+
+  def get_e_to(self):
+    return self.e_to
+    
+  def get_w_to(self):
+    return self.w_to
+
+  # Rooms able to hold multiple items
